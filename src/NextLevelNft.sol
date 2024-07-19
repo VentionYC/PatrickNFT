8674f54ffc7d3765b571bd8956cfbc56339bc985
Vention Young <backstreetal6year@gmail.com>
2024-07-19 03:07:39 +0000

    github failed

--- src/NextLevelNft.sol
+++ src/NextLevelNft.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NextLevelNft is ERC721{
-    constructor() ERC721("NextLevelNft", "NLN"){
-        
-    }
+contract NextLevelNft is ERC721 {
+    constructor() ERC721("NextLevelNft", "NLN") {}
 }
