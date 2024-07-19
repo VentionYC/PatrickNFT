3ce47677a50b46b3a6874e24d4cdac88ab7e6662
Vention Young <backstreetal6year@gmail.com>
2024-07-19 02:59:22 +0000

    NLN naming init

---
+++ src/NextLevelNft.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NextLevelNft is ERC721{
+    constructor() ERC721("NextLevelNft", "NLN"){
+        
+    }
+}
