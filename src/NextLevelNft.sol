34604eb497e1d6ca39976b2d5c4061707b8bbc7f
Vention Young <backstreetal6year@gmail.com>
2024-07-22 07:32:43 +0000

    Next level nft basic setup

--- src/NextLevelNft.sol
+++ src/NextLevelNft.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 //Test commit
 contract NextLevelNft is ERC721 {
-    constructor() ERC721("NextLevelNft", "NLN") {}
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("NextLevelNft", "NLN") {
+        s_tokenCounter = 0;
+    }
+
+    // let the people choose each their own token uri, or say how the toke looked like 
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    //tokenid endpoint api return the metadata of the NFT
+    function tokenURI(uint256 tokenId) public view override returns (string memory){
+        return s_tokenIdToUri[tokenId];
+    } 
+    
 }
