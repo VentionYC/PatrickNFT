204a59a350e8418a4143d1715cb6351c9e0a184c
Vention Young <backstreetal6year@gmail.com>
2024-07-22 07:41:22 +0000

    basic deploy script

---
+++ script/DeployNft.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+import {Script} from "forge-std/Script.sol";
+import {NextLevelNft} from "../src/NextLevelNft.sol";
+contract DeployNextLevelNft is Script {
+
+    
+    function run() external returns (NextLevelNft) {
+        vm.startBroadcast();
+            NextLevelNft nln = new NextLevelNft();
+        vm.stopBroadcast();
+        return nln;
+    }
+    
+}
