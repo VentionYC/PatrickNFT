5d6743a2e18d2bbad4ba786c203e4e20b3afa6fc
Vention Young <backstreetal6year@gmail.com>
2024-07-23 07:16:44 +0000

    three diff status for the progress nft

--- src/ProgressNft.sol
+++ src/ProgressNft.sol
@@ -2,7 +2,20 @@
 pragma solidity ^0.8.25;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract ProgressNft is ERC721 {
-    constructor() ERC721("Progress NFT", "PN"){
-        
+    uint256 private s_tokenCounter;
+    string private s_trackingSvg;
+    string private s_hitSvg;
+    string private s_hitHitSvg;
+
+    //we need to bring in the svg file
+    constructor(string memory trackingSvg,
+                string memory hitSvg,
+                string memory hitHitSvg) ERC721("Progress NFT", "PN"){
+        s_tokenCounter = 0;
+        s_trackingSvg = trackingSvg;
+        s_hitSvg = hitSvg;
+        s_hitHitSvg = hitHitSvg;
     }
+
+    function mintNft()
 }
